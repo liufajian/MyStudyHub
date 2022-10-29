83b1028001fe3c13d1c4a615421a763de5b8294d
liufajian <379002060@qq.com>
2022-10-29 15:36:41 +0800

    RuntimeBuilder

---
+++ dotnet/RuntimeBuilder/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RuntimeBuilder;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+MyClassBuilder MCB = new MyClassBuilder("Student");
+var myclass = MCB.CreateObject(new string[3] { "ID", "Name", "Address" }, new Type[3] { typeof(int), typeof(string), typeof(string) });
+Type TP = myclass.GetType();
+
+foreach (PropertyInfo PI in TP.GetProperties())
+{
+    Console.WriteLine(PI.Name);
+}
+
+Console.ReadLine();
