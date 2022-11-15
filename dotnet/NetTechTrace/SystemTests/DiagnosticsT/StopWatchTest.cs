b1ed86cb4d3db0fb2d469e2ad88522e08f639d83
liufajian <379002060@qq.com>
2022-11-15 16:33:03 +0800

    StopWatchTest

--- dotnet/NetTechTrace/SystemTests/DiagnosticsT/StopWatchTest.cs
+++ dotnet/NetTechTrace/SystemTests/DiagnosticsT/StopWatchTest.cs
@@ -14,10 +14,34 @@ namespace NetTechTrace.SystemTests.DiagnosticsT
             {
                 Thread.Sleep(1000);
 
-                Assert.IsTrue(sw.ElapsedMilliseconds > i * 1000 && sw.ElapsedMilliseconds < i * 1000 + 100);
+                Assert.IsTrue(sw.ElapsedMilliseconds > i * 1000 && sw.ElapsedMilliseconds < i * 1015);
             }
 
-            Assert.IsTrue(sw.ElapsedMilliseconds > 10 * 1000 && sw.ElapsedMilliseconds < 10 * 1000 + 100);
+            Assert.IsTrue(sw.ElapsedMilliseconds > 10 * 1000 && sw.ElapsedMilliseconds < 10 * 1015);
+
+            Console.WriteLine("验证成功");
+        }
+
+        [TestMethod]
+        public void TestTimeout()
+        {
+            var timeoutSeconds = 30;
+            var timeoutSpan = TimeSpan.FromSeconds(timeoutSeconds);
+            var sw = Stopwatch.StartNew();
+
+            for (var i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(1001);
+
+                if (sw.Elapsed > timeoutSpan)
+                {
+                    sw.Stop();
+                    Assert.AreEqual(i, 30);
+                    break;
+                }
+            }
+
+            Assert.IsTrue(sw.ElapsedMilliseconds > 30 * 1000 && sw.ElapsedMilliseconds < 30 * 1015);
 
             Console.WriteLine("验证成功");
         }
