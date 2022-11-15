384fe946968c6f4d7997d1c72daba5980ffcb552
liufajian <379002060@qq.com>
2022-11-15 15:29:33 +0800

    StopWatchTest

---
+++ dotnet/NetTechTrace/SystemTests/DiagnosticsT/StopWatchTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace NetTechTrace.SystemTests.DiagnosticsT
+{
+    [TestClass]
+    public class StopWatchTest
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var sw = Stopwatch.StartNew();
+
+            for (var i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(1000);
+
+                Assert.IsTrue(sw.ElapsedMilliseconds > i * 1000 && sw.ElapsedMilliseconds < i * 1000 + 100);
+            }
+
+            Assert.IsTrue(sw.ElapsedMilliseconds > 10 * 1000 && sw.ElapsedMilliseconds < 10 * 1000 + 100);
+
+            Console.WriteLine("验证成功");
+        }
+    }
+}
