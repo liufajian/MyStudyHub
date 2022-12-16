ac9f83012eaa6ef24f0d3804e519a08db41211e8
liufajian <liufajian@msn.com>
2022-12-16 15:54:21 +0800

    WebApiDemo

---
+++ dotnet/WebApiDemo/AspNetCustoms/JsonBodyBinderProvider.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.Text.Json;
+
+namespace WebApiDemo.AspNetCustoms
+{
+    /// <summary>
+    /// application/json模型绑定(支持空body，避免报400 error:A non-empty request body is required.)
+    /// </summary>
+    internal class JsonBodyBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (!context.Metadata.ModelType.IsValueType)
+            {
+                return new BinderTypeModelBinder(typeof(EmptyBodyModelBinder));
+            }
+
+            return null;
+        }
+
+        class EmptyBodyModelBinder : IModelBinder
+        {
+            public async Task BindModelAsync(ModelBindingContext bindingContext)
+            {
+                if (bindingContext.HttpContext.Request.ContentType?.Contains("application/json") != true)
+                {
+                    return;
+                }
+
+                var stream = bindingContext.HttpContext.Request.Body;
+                using var reader = new StreamReader(stream);
+                var jsonbody = await reader.ReadToEndAsync();
+                if (jsonbody.Length > 0)
+                {
+                    var obj = JsonSerializer.Deserialize(jsonbody, bindingContext.ModelType);
+                    bindingContext.Result = ModelBindingResult.Success(obj);
+                }
+                else
+                {
+                    bindingContext.Result = ModelBindingResult.Success(null);
+                }
+            }
+        }
+    }
+}
---
+++ dotnet/WebApiDemo/AspNetCustoms/LoadUserMiddleware.cs
@@ -0,0 +1,125 @@
+﻿using System.Security.Principal;
+
+namespace WebApiDemo.AspNetCustoms
+{
+    public class LoadUserMiddleware
+    {
+        readonly RequestDelegate next;
+
+        public LoadUserMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        /// <summary>
+        /// 请求拦截处理
+        /// </summary>
+        /// <param name="context">HTTP请求</param>
+        /// <returns></returns>
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.User.Identity?.IsAuthenticated == true)
+            {
+                var clientId = GetValueFromClaims(context, "clientId");
+                _ = int.TryParse(GetValueFromClaims(context, "userId"), out int userId);
+                var userName = GetValueFromClaims(context, "userName");
+
+                context.Items["ClientId"] = clientId;
+                var userinfo = new AuthUserInfo(userId, userName)
+                {
+                    IpAddress = GetClientIpAddress(context)
+                };
+                context.User = new GenericPrincipal(userinfo, null);
+            }
+            await next(context);
+        }
+
+        private static string GetValueFromClaims(HttpContext context, string claimKey)
+        {
+            var claim = context.User.FindFirst(claimKey);
+            if (claim != null)
+            {
+                return claim.Value;
+            }
+            return string.Empty;
+        }
+
+        private static string GetClientIpAddress(HttpContext context)
+        {
+            return context.Connection.RemoteIpAddress?.ToString() + context.Connection.RemotePort;
+        }
+
+        /// <summary>
+        /// 用户信息
+        /// </summary>
+        public class OptUserInfo
+        {
+            /// <summary>
+            /// 用户ID
+            /// </summary>
+            public int UserId { get; set; }
+
+            /// <summary>
+            /// 用户名称
+            /// </summary>
+            public string UserName { get; set; }
+
+            /// <summary>
+            /// 用户组
+            /// </summary>
+            public string UserGroup { get; set; }
+
+            /// <summary>
+            /// IP地址
+            /// </summary>
+            public string IpAddress { get; set; }
+
+            protected OptUserInfo()
+            {
+
+            }
+
+            public OptUserInfo(int userId, string userName)
+            {
+                UserId = userId;
+                UserName = userName;
+            }
+
+            public OptUserInfo Clone()
+            {
+                return (OptUserInfo)MemberwiseClone();
+            }
+
+            /// <summary>
+            /// 最小权限系统用户
+            /// </summary>
+            public static readonly OptUserInfo SystemUser;
+
+            public static readonly OptUserInfo UnitTestUser;
+
+            static OptUserInfo()
+            {
+                SystemUser = new OptUserInfo(0, "系统");
+                UnitTestUser = new OptUserInfo(0, "单元测试");
+            }
+        }
+
+        public class AuthUserInfo : OptUserInfo, IIdentity
+        {
+            /// <summary>
+            /// 用户名
+            /// </summary>
+            public string Name => UserName;
+
+            public string AuthenticationType => "JWT";
+
+            public bool IsAuthenticated => true;
+
+            public AuthUserInfo(int userId, string userName)
+                : base(userId, userName)
+            {
+
+            }
+        }
+    }
+}
